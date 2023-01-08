8aba5a0c53bcb4a6d58694530df09958d40180e1
Mathias FAURE <mathias.faure.31@gmail.com>
2023-01-08 11:19:36 +0100

    Spring initilializr

---
+++ src/test/java/fr/mujum/springGpt/SpringGptApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mujum.springGpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
